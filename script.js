73ce744a5e679622f2674061722cc0a59aa4f453
Yavor Rashev <61455307+yavor-r@users.noreply.github.com>
2021-09-04 22:55:05 +0300

    js commit

--- script.js
+++ script.js
@@ -1,2 +1,5 @@
-import json from 'currencies.json'; //getting the json
-console.log(json);
+$.getJSON('https://api.jsonbin.io/b/6133ce8f470d33259402768b', function (currencies) {
+    // JSON result in 'currencies' variable
+});
+
+console.log(currencies);
