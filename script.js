ac60b81c9f41194fb1cb7208bc9bb07f3566e807
Yavor Rashev <61455307+yavor-r@users.noreply.github.com>
2021-09-04 03:05:46 +0300

    script commit and push

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
-$.getJSON("currencies.json", function (json) {
-    console.log(json); // this will show the info it in firebug console
-});
+fetch("test.json")
+    .then(response => response.json())
+    .then(json => console.log(json));
