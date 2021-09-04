c00664271bcd4ddc65cfa44bbee217f1b25e954d
Yavor Rashev <61455307+yavor-r@users.noreply.github.com>
2021-09-04 23:12:54 +0300

    js commit

--- script.js
+++ script.js
@@ -1,14 +1,6 @@
-fetchCurrencies(callback) {
-    fetch(`https://api.jsonbin.io/b/6133ce8f470d33259402768b`)
-        .then(response => response.json())
-        .then(json => callback(null, json.currencies)
-        .catch(error => callback(error, null))
+async function fetchDataAsync(url) {
+    const response = await fetch(url);
+    console.log(await response.json());
 }
 
-fetchCurrencies((error, currencies)=> {
-    if (error)
-        console.log(error)
-    else
-        console.log(currencies[])
-
-});
+fetchDataAsync('https://api.jsonbin.io/b/6133ce8f470d33259402768b');
