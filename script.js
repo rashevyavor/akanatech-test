c3b33baa7cb8c06153891324a4f95913f18cbd7d
Yavor Rashev <61455307+yavor-r@users.noreply.github.com>
2021-09-05 22:30:02 +0300

    final commits to html and js

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
-fetch('https://json.extendsclass.com/bin/2a71a3926323')
-    .then(response => response.json())
-    .then(data => console.log(data));
+fetch('https://api.jsonbin.io/b/6134fc2885791e1732a056ee')
+    .then((response) => {
+        if (response.ok) {
+            return response.json();
+        } else {
+            throw new Error("NETWORK RESPONSE ERROR");
+        }
+    })
+    .then(data => {
+        console.log(data);
+        displayRates(data)
+    })
+    .catch((error) => console.error("FETCH ERROR:", error));
+
+function displayRates(data) {
+    const myObj = JSON.parse(JSON.stringify(data));
+    document.getElementById("currencyData").innerHTML = (myObj);
+    console.log(myObj);
+}
